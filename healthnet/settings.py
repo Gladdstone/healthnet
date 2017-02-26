98ffe743168ea5b1d5429df32b87bd248a1ce07c
Joe Farrell <jdf4921@rit.edu>
2017-02-25 21:56:02 -0500

    Index and login
    Added LOGIN_REDIRECT_URL. Created and populated catalog. Created
    index.html and login.html. Initialized corresponding stylesheets,
    implementation incomplete.

--- healthnet/settings.py
+++ healthnet/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'index'
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
