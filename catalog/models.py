cb4d67cec6b97e76fa1664341c22e187e2fdae86
Joe Farrell <jdf4921@rit.edu>
2017-02-26 11:03:33 -0500

    Base Models
    Created classes Patient, Doctor, and Nurse in catalog/models.py.
    Migrations made. Database updated. Methods, field, and metadata are
    subject to change in future releases.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+
+    # Fields
+    fname = models.CharField(max_length = 20, help_text = 'First name')
+    lname = models.CharField(max_length = 20, help_text = 'Last name')
+    email = models.EmailField(help_text = 'Email')
+    dob = models.DateField(help_text = 'Date of Birth')
+    ssn = models.CharField(max_length = 9, help_text = 'Social Security Number')
+    address = models.TextField(max_length = 40, help_text = 'Address')
+    city = models.CharField(max_length = 20, help_text = 'City')
+    zip = models.CharField(max_length = 5, help_text = 'Zip Code')
+
+    # Metadata
+    class Meta:
+        ordering = ['lname', '-dob', 'ssn']
+
+    # Methods
+    def __str__(self):
+        return self.lname
+
+class Doctor(models.Model):
+
+    # Fields
+    fname = models.CharField(max_length=20, help_text='First name')
+    lname = models.CharField(max_length=20, help_text='Last name')
+    email = models.EmailField(help_text='Email')
+    dob = models.DateField(help_text='Date of Birth')
+
+    # Metadata
+    class Meta:
+        ordering = ['lname', '-dob']
+
+    # Methods
+    def __str__(self):
+        return self.lname
+
+class Nurse(models.Model):
+
+    # Fields
+    fname = models.CharField(max_length=20, help_text='First name')
+    lname = models.CharField(max_length=20, help_text='Last name')
+    email = models.EmailField(help_text='Email')
+    dob = models.DateField(help_text='Date of Birth')
+
+    # Metadata
+    class Meta:
+        ordering = ['lname', '-dob']
+
+    # Methods
+    def __str__(self):
+        return self.lname
